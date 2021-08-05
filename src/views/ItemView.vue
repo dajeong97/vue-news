78f9853ccd38607b5d4bf70c43264cc2c49dd166
KimDajeong <cgscwg@gmail.com>
2021-08-05 20:44:29 +0900

    ItemView 스타일링, transition 적용

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
-    <p> {{ this.$store.state.item.title}} </p>
-    <div>
-      {{ this.$store.state.item.content}} 
-    </div>
+    <section>
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link :to="`/user/${fetchedItem.user}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ fetchedItem.title }}</h2>
+    </section>
+    <section>
+      <div v-html="fetchedItem.content">
+        {{ fetchedItem.content}}
+        </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
+  computed: {
+    ...mapGetters(['fetchedItem'])
+  },
   created() {
     const itemId = this.$route.params.id;
     this.$store.dispatch('FETCH_ITEM', itemId);
@@ -16,6 +37,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  padding: 0.5rem;
+  display: flex;
+  align-items: center;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
 
 </style>
