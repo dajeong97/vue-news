41268a4ba6e39d07f4b0c7d32bc463d30b064a71
KimDajeong <cgscwg@gmail.com>
2021-08-05 18:52:03 +0900

    store 모듈화 및 연동

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
-    <div v-for="item in ask">{{ item.title }}</div>
+    <p v-for="item in fetchedAsk">
+      <a v-bind:href="item.url">
+        {{ item.title }}
+      </a>
+      <small>{{ item.time_ago }} by {{ item.user }}</small>
+    </p>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js';
+import { mapGetters } from 'vuex'
 
 export default {
-  data() {
-    return {
-      ask: []
-    }
+  computed: {
+    ...mapGetters([
+      'fetchedAsk'
+    ])
   },
   created() {
-    fetchAskList()
-      .then(response => this.ask = response.data)
-      .catch(error => console.log(error))
+    this.$store.dispatch('FETCH_ASK');
   }
 }
 </script>
