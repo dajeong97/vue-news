febdf15b388566edffe58da893616a7b057c91fe
KimDajeong <cgscwg@gmail.com>
2021-08-08 19:17:59 +0900

    UserProfile 컴포넌트 공통화

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,33 +1,32 @@
 <template>
   <div>
     <section>
-      <div class="user-container">
-        <div>
-          <i class="fas fa-user"></i>
-        </div>
-        <div class="user-description">
-          <router-link :to="`/user/${fetchedItem.user}`">
-            {{ fetchedItem.user }}
-          </router-link>
-          <div class="time">
-            {{ fetchedItem.time_ago }}
-          </div>
-        </div>
-      </div>
+      <!-- 사용자 정보 -->
+      <user-profile :info="fetchedItem">
+        <router-link slot="username" :to="`/user/${fetchedItem.user}`">
+          {{ fetchedItem.user }}
+        </router-link>
+        <template slot="time">{{ 'Posted ' + fetchedItem.time_ago }}</template>
+      </user-profile>
+    </section>
+    <section>
       <h2>{{ fetchedItem.title }}</h2>
     </section>
     <section>
-      <div v-html="fetchedItem.content">
-        {{ fetchedItem.content}}
-        </div>
+      <!-- 질문 댓글 -->
+      <div v-html="fetchedItem.content"></div>
     </section>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import UserProfile from '../components/UserProfile.vue';
 
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     ...mapGetters(['fetchedItem'])
   },
@@ -37,21 +36,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.user-container {
-  padding: 0.5rem;
-  display: flex;
-  align-items: center;
-}
-.fa-user {
-  font-size: 2.5rem;
-}
-.user-description {
-  padding-left: 8px;
-}
-.time {
-  font-size: 0.7rem;
-}
-
-</style>
